c1363f8adb9161f1257a4f7aebe9cca77b24e9b3
unknown <cuicui.zhu@ZHU-CUI-CUI.im20-group.com>
2015-12-07 17:07:56 +0800

    add files

---
+++ win.js
@@ -0,0 +1,34 @@
+var win=function(){
+    this.cfg={
+        title:"对话框",
+        width:200,
+        height:100,
+        btnSure:"确定",
+        btnCancle:"取消",
+        btnSureHandler:null,
+        btnCancleHandler:null
+    };
+    this.container=$('<div class="container"></div>');
+    this.handlers={};
+}
+win.prototype={
+    constructor:win,
+    show:function(options,content){
+      this.cfg= $.extend(this.cfg);
+      var html=this.container.append(options.content);
+      $("body").append(html);
+    },
+    onEvent:function(type,handler) {
+        if (!this.handlers[type]) {
+            this.handlers[type] = [];
+        }
+        this.handlers[type].push(handler);
+    },
+    fireEvent:function(type,data){
+          if(this.handlers[type] instanceof Array){
+              for(var i=0;i<this.handlers[type].length;i++){
+                  this.handlers[type][i](data);
+              }
+          }
+    }
+}
